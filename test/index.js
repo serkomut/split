f19f83bb4cfb14b46b5721c813dae186be32d599
serkomut <serkomut@gmail.com>
2015-01-08 23:55:00 +0200

    gitignore update

--- test/index.js
+++ test/index.js
@@ -1,38 +1,47 @@
 var should = require('chai').should(),
-serkomut = require('../index'),
-escape = serkomut.escape,
-unescape = serkomut.unescape;
+    serkomut = require('../index'),
+    index = serkomut.spliter,
+    escape = serkomut.escape,
+    unescape = serkomut.unescape;
+
+describe("#index", function() {
+    it("List : ", function() {
+        var result = index('zaman, hayat, falan', ',');
+        result[0].should.equal('zaman');
+    });
+});
+
 describe('#escape', function() {
-it('converts & into &amp;', function() {
-escape('&').should.equal('&amp;');
-});
-it('converts " into &quot;', function() {
-escape('"').should.equal('&quot;');
-});
-it('converts \' into &#39;', function() {
-escape('\'').should.equal('&#39;');
-});
-it('converts < into &lt;', function() {
-escape('<').should.equal('&lt;');
-});
-it('converts > into &gt;', function() {
-escape('>').should.equal('&gt;');
-});
+    it('converts & into &amp;', function() {
+        escape('&').should.equal('&amp;');
+    });
+    it('converts " into &quot;', function() {
+        escape('"').should.equal('&quot;');
+    });
+    it('converts \' into &#39;', function() {
+        escape('\'').should.equal('&#39;');
+    });
+    it('converts < into &lt;', function() {
+        escape('<').should.equal('&lt;');
+    });
+    it('converts > into &gt;', function() {
+        escape('>').should.equal('&gt;');
+    });
 });
 describe('#unescape', function() {
-it('converts &amp; into &', function() {
-unescape('&amp;').should.equal('&');
-});
-it('converts &quot; into "', function() {
-unescape('&quot;').should.equal('"');
-});
-it('converts &#39; into \'', function() {
-unescape('&#39;').should.equal('\'');
-});
-it('converts &lt; into <', function() {
-unescape('&lt;').should.equal('<');
-});
-it('converts &gt; into >', function() {
-unescape('&gt;').should.equal('>');
-});
+    it('converts &amp; into &', function() {
+        unescape('&amp;').should.equal('&');
+    });
+    it('converts &quot; into "', function() {
+        unescape('&quot;').should.equal('"');
+    });
+    it('converts &#39; into \'', function() {
+        unescape('&#39;').should.equal('\'');
+    });
+    it('converts &lt; into <', function() {
+        unescape('&lt;').should.equal('<');
+    });
+    it('converts &gt; into >', function() {
+        unescape('&gt;').should.equal('>');
+    });
 });
